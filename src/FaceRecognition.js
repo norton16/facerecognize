ce81045dca221ac402576f7fc8daa984af512251
Brian Norton <33581086+norton16@users.noreply.github.com>
2019-10-16 18:43:20 -0400

    Add files via upload

--- src/FaceRecognition.js
+++ src/FaceRecognition.js
@@ -4,7 +4,7 @@ import './FaceRecognition.css';
 const FaceRecognition = ({ imageUrl, box }) => {
     return (
        <div class='content'>
-           <div className='absolute mt2'>
+           <div className='absolute mt2' id='result'>
            <img id='inputimage' src={imageUrl} alt='' width='300px' height='auto'></img>
            <div className='bounding-box' 
            style={{top: box.topRow, right:box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
--- src/ImageLinkForm.js
+++ src/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './ImageLinkForm.css'
 
 const ImageLinkForm = ({ onInputChange, onSubmit }) => {
     return (
-        <div class="content">
+        <div class="content" id="linkform">
             <p className = 'f4'>{'Enter a link to a picture!'}</p>
                 <div className='center pa4 br3' class='center'>
                 <input className='f4 pa2 ba bb1 center' class='form' type='text' onChange={onInputChange}/>
